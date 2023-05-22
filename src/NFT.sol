1c6458b82c19a4b4099388747a494a12b09cd2b6
Ashish Binu <ashishbinu90@gmail.com>
2023-05-22 11:12:15 +0530

    fix(pragma): lock compiler version

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
 import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
