90531742e93e9c79a609781ccac2c6c2aca6c99c
Ashish Binu <ashishbinu90@gmail.com>
2023-05-28 19:54:49 +0530

    refactor: convert interface to local import calls

--- src/MainFactory.sol
+++ src/MainFactory.sol
@@ -3,46 +3,32 @@ pragma solidity 0.8.17;
 
 import {Clones} from "openzeppelin-contracts/contracts/proxy/Clones.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
-
-interface INFT {
-    function safeMint(address _to, string memory _uri) external returns (uint256);
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external;
-    function approve(address _to, uint256 _tokenId) external;
-    function owner() external returns (address);
-}
-
-interface IMultiSigWallet {
-    function initialize(address[] memory _owners, uint256 _numConfirmationsRequired) external;
-    function submitTransaction(address _to, uint256 _value, bytes memory _data) external;
-    function executeTransaction(uint256 _txIndex) external;
-    function revokeConfirmation(uint256 _txIndex) external;
-    function getOwners() external view returns (address[] memory);
-    function getTransactionCount() external view returns (uint256);
-}
+import {NFT} from "./NFT.sol";
+import {MultiSigWallet} from "./MultiSigWallet.sol";
 
 contract MainFactory is Ownable {
-    address public certificateNFTAddress;
-    address public multiSigWalletAddress;
+    NFT public nft;
+    MultiSigWallet public multiSigWallet;
 
     mapping(address => address[]) public multiSigWalletsOf;
 
     constructor(address _certificateNFTAddress, address _multiSigWalletAddress) {
-        certificateNFTAddress = _certificateNFTAddress;
-        multiSigWalletAddress = _multiSigWalletAddress;
+        nft = NFT(_certificateNFTAddress);
+        multiSigWallet = MultiSigWallet(payable(_multiSigWalletAddress));
     }
 
     function setCertificateNFTAddress(address _addr) external onlyOwner {
-        certificateNFTAddress = _addr;
+        nft = NFT(_addr);
     }
 
     function setMultiSigWalletAddress(address _addr) external onlyOwner {
-        multiSigWalletAddress = _addr;
+        multiSigWallet = MultiSigWallet(payable(_addr));
     }
 
     function issueCertificate(address _to, string memory _uri) external {
         require(_to != address(0), "MainFactory: Can't issue certificate to address(0)");
-        uint256 tokenId = INFT(certificateNFTAddress).safeMint(msg.sender, _uri);
-        INFT(certificateNFTAddress).safeTransferFrom(msg.sender, _to, tokenId);
+        uint256 tokenId = nft.safeMint(msg.sender, _uri);
+        nft.safeTransferFrom(msg.sender, _to, tokenId);
     }
 
     // TODO: burn certificate
@@ -52,8 +38,8 @@ contract MainFactory is Ownable {
         external
         returns (address)
     {
-        address proxy = Clones.clone(multiSigWalletAddress);
-        IMultiSigWallet(proxy).initialize(_owners, _numConfirmationsRequired);
+        address proxy = Clones.clone(address(multiSigWallet));
+        MultiSigWallet(payable(proxy)).initialize(_owners, _numConfirmationsRequired);
 
         for (uint256 i = 0; i < _owners.length; i++) {
             multiSigWalletsOf[_owners[i]].push(proxy);
