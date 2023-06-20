9c4ee556a0f3ba79a9d416eeb69a6423971bbdb4
Ashish Binu <ashishbinu90@gmail.com>
2023-06-20 18:43:36 +0530

    fix: Correct issuance check in _issue function
    Ensures token is not already issued by inverting the "require" condition in _issue function.

--- src/token/ERC5484.sol
+++ src/token/ERC5484.sol
@@ -82,7 +82,7 @@ contract ERC5484 is Context, ERC165, ERC721, IERC5484 {
     }
 
     function _issue(address from, address to, uint256 tokenId, BurnAuth burnAuth_) internal virtual isIssuing {
-        require(_issued[tokenId], "ERC5484: token is already issued");
+        require(!_issued[tokenId], "ERC5484: token is already issued");
         // FIX: require(_issuanceApprovalOf[tokenId][to], "ERC5484: token is not approved for issuance by receiver");
 
         // TODO: burnAuth SHALL be presented to receiver before issuance.
