e8eb1d64e288547d0dc79f26658d16ae74157955
Ashish Binu <ashishbinu90@gmail.com>
2023-06-22 08:29:40 +0530

    refactor: change import to @openzeppelin

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {Initializable} from "openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 contract MultiSigWallet is Initializable {
     struct Transaction {
